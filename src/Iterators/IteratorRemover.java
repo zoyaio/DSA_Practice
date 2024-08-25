87f85bea9dcc944d414f0951755b7c4b571a7da4
MrT-Branham <sturner@cuhsd.org>
2024-08-24 18:09:06 -0700

    Added missing iterator files

---
+++ src/Iterators/IteratorRemover.java
@@ -0,0 +1,27 @@
+package Iterators;//(c) A+ Computer Science
+//www.apluscompsci.com
+
+import java.util.ArrayList;
+
+public class IteratorRemover
+{
+	private ArrayList<String> list;
+	private String toRemove;
+
+	public IteratorRemover(String line, String rem)
+	{
+	}
+
+	public void setTest(String line, String rem)
+	{
+	}
+
+	public void remove()
+	{
+	}
+
+	public String toString()
+	{
+		return "";
+	}
+}
---
+++ src/Iterators/IteratorReplacer.java
@@ -0,0 +1,27 @@
+package Iterators;//(c) A+ Computer Science
+//www.apluscompsci.com
+
+import java.util.ArrayList;
+
+public class IteratorReplacer
+{
+	private ArrayList<String> list;
+	private String toRemove, replaceWith;
+
+	public IteratorReplacer(String line, String rem, String rep)
+	{
+	}
+
+	public void setEmAll(String line, String rem, String rep)
+	{
+	}
+
+	public void replace()
+	{
+	}
+
+	public String toString()
+	{
+		return list.toString()+"\n\n";
+	}
+}
