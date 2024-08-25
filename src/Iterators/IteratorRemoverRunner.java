fb1868b84cc1569858b90960759f763cd0faf0ec
MrT-Branham <sturner@cuhsd.org>
2024-08-24 18:21:01 -0700

    Updated the runners with test cases

--- src/Iterators/IteratorRemoverRunner.java
+++ src/Iterators/IteratorRemoverRunner.java
@@ -5,6 +5,16 @@ public class IteratorRemoverRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases	
+		IteratorRemover test = new IteratorRemover("a b c a b c","a");
+		test.remove();
+		System.out.println(test);
+
+		test.setTest("a b c d e f g h i j x x x x","x");
+		test.remove();
+		System.out.println(test);
+
+		test.setTest("1 2 3 4 5 6 a b c a b c","b");
+		test.remove();
+		System.out.println(test);
 	}
 }
--- src/Iterators/IteratorReplacerRunner.java
+++ src/Iterators/IteratorReplacerRunner.java
@@ -5,6 +5,16 @@ public class IteratorReplacerRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases		
+		IteratorReplacer test = new IteratorReplacer("a b c a b c","a","+");
+		test.replace();
+		System.out.println(test);
+
+		test.setTest("a b c d e f g h i j x x x x","x","7");
+		test.replace();
+		System.out.println(test);
+
+		test.setTest("1 2 3 4 5 6 a b c a b c","b","#");
+		test.replace();
+		System.out.println(test);
 	}
 }
