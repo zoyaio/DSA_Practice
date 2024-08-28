27b317af27cc3c1ff48dd6c6d63829a1c8469c76
Steven Turner <52668009+MrT-Branham@users.noreply.github.com>
2024-08-28 09:56:22 -0700

    Update GradeBookRunner.java

--- src/sortsearch/gradebook/GradeBookRunner.java
+++ src/sortsearch/gradebook/GradeBookRunner.java
@@ -8,44 +8,6 @@ public class GradeBookRunner
 {
    public static void main( String args[] ) throws Exception
    {
-		System.out.println("Welcome to the Class Stats program!");
-		
-		Scanner kb = new Scanner( System.in );
-		
-		System.out.print("Enter the class name : ");
-		String className = kb.nextLine();
-		
-		System.out.print("Enter the number of students : ");
-		int numStudents = kb.nextInt();
-		kb.nextLine();
-		
-		Class theClass = new Class(className, numStudents);
-		
-		for(int i=0; i<numStudents; i++)
-		{
-			System.out.print("Enter a student name : ");			
-			String stuName = kb.nextLine();
-			
-			System.out.print("Enter the number of grades : ");
-			int c = kb.nextInt();
-			
-			double[] g = new double[c];
-			for( int x = 0; x < c; x++ )
-			{
-				System.out.print("Enter a grade : ");	
-				g[x] = kb.nextDouble();
-			}				
-						
-			theClass.addStudent(i,new Student(stuName, g));
-			kb.nextLine();
-		}
-
-		System.out.println("\n\n"+theClass);
-		
-		System.out.println("Failure List = " + theClass.getFailureList(70));	
-		System.out.println("Highest Average = " + theClass.getStudentWithHighestAverage());
-		System.out.println("Lowest Average = " + theClass.getStudentWithLowestAverage());
-		
-		System.out.println("Class average = " + theClass.getClassAverage());
-	}		
+	   //Finish the method
+   }	
 }
