9737fd57312ca85ab8810618b4b77e5c3da8844c
zoya b <74390131+zoyaio@users.noreply.github.com>
2025-03-27 07:44:32 -0700

    graph labs 2

--- src/graphs/graph_simple2/BiDirectionalGraph.java
+++ src/graphs/graph_simple2/BiDirectionalGraph.java
@@ -3,29 +3,95 @@ package graphs.graph_simple2;//(c) A+ Computer Science
 
 //Name -
 
-import java.util.TreeMap;
-import java.util.TreeSet;
+import java.util.*;
 
 public class BiDirectionalGraph
 {
-	private TreeMap<String, TreeSet<String>> map;
+	private TreeMap<String, HashSet<String>> map;
 	private boolean found;
+	HashSet<String> visit;
 
 	public BiDirectionalGraph(String line)
 	{
+		//point map at a new hash map
+		map = new TreeMap<>();
+
+		String[] list = line.split(" ");
+
+		for (int i = 0; i < list.length; i +=2) {
+			addToMap(map, list[i], list[i+1]);
+			addToMap(map, list[i+1], list[i]);
+		}
+
+		//point visit a new hash set
+		visit = new HashSet<>();
+		found = false;
 	}
 
-	public boolean contains(String name)
+	private void addToMap(Map<String, HashSet<String>> map,
+						  String C, String D) {
+
+		if (!map.containsKey(C)) {
+			map.put(C, new HashSet<>());
+		}
+		map.get(C).add(D);
+	}
+
+
+	public boolean contains(String letter)
 	{
+		if(map.get(letter)==null)
+			return false;
 		return true;
 	}
 
-	public void check(String first, String second, TreeSet<String> placedUsed)
+	public void check(String first, String second)
 	{
+		visit.add(first);
+
+		if (first.equals(second)) {
+			found = true;
+			return;
+		}
+		// loops through each of the connections of the map
+		for (String nodeToVisit : map.get(first)) {
+			if (!visit.contains(nodeToVisit)) {
+				visit.add(nodeToVisit);
+				check(nodeToVisit, second);
+				if (found) {
+					return;
+				}
+			}
+		}
+		found = false;
 	}
 
 	public String toString()
 	{
-		return "";
+		if(found)
+			return "YAY";
+		return "NAY";
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//if first equals second
+//done
+//else
+//get all of first's connections
+//loop the list of connections for first
+//if current node not yet visited
+//add node to visited
+//recursive call
--- src/graphs/graph_simple2/BiDirectionalGraphRunner.java
+++ src/graphs/graph_simple2/BiDirectionalGraphRunner.java
@@ -3,11 +3,35 @@ package graphs.graph_simple2;//(c) A+ Computer Science
 
 //Name -
 
+import graphs.graph_simple.Graph;
+
+import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
+
+import static java.lang.System.out;
 
 public class BiDirectionalGraphRunner
 {
 	public static void main( String[] args ) throws IOException
 	{
+		Scanner file = new Scanner(new File("C:\\Users\\zoya\\IdeaProjects\\DSA_Practice_gradebook\\src\\graphs\\graph_simple2\\bidgraph.dat"));
+		int howManyTimes = file.nextInt();
+		file.nextLine();
+
+		for(int x=0; x<howManyTimes; x++)
+		{
+			String input = file.nextLine();
+			BiDirectionalGraph test = new BiDirectionalGraph(input);
+			input=file.nextLine();
+			String[] inputSplit = input.split(" ");
+			String start = inputSplit[0];
+			String stop = inputSplit[1];
+
+			if(test.contains(start)&&test.contains(stop))
+				test.check(start,stop);
+
+			out.println(start + " CONNECTS TO " + stop + " == " + test);
+		}
 	}
-}
+	}
