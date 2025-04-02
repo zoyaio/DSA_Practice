c5c23eaa7d0ff9cca345fffc02d9cd4773c5844f
zoya b <74390131+zoyaio@users.noreply.github.com>
2025-04-01 18:45:41 -0700

    graphs lab 4 almost working

--- src/graphs/graph_all_paths/AllPathsGraph.java
+++ src/graphs/graph_all_paths/AllPathsGraph.java
@@ -2,9 +2,7 @@ package graphs.graph_all_paths;//(c) A+ Computer Science
 //www.apluscompsci.com
 //Name -
 
-import java.util.Map;
-import java.util.Set;
-import java.util.ArrayList;
+import java.util.*;
 
 public class AllPathsGraph
 {
@@ -12,25 +10,62 @@ public class AllPathsGraph
 	private boolean found;
 	private int shortest;
 	private ArrayList<String> paths;
-	private Set<String> visit;
+	private Set<String> visited;
 
 	public AllPathsGraph(String line)
 	{
+		map = new HashMap<>();
+
+
 		//build your map / graph
+		String[] edges = line.split(" ");
+		for (String pair : edges) {
+			String A = pair.substring(0,1);
+			String B = pair.substring(1);
+			if (!map.containsKey(A)) {
+				map.put(A, new TreeSet<String>() );
+			}
+			if (!map.containsKey(B)) {
+				map.put(B, new TreeSet<String>() );
+			}
+			map.get(A).add(B);
+			map.get(B).add(A);
+
+		}
 		//initialize all of your instance variables
+		found = false;
+		shortest = 0;
+		paths = new ArrayList<>();
+		visited = new TreeSet<>();
+	}
+	public void check (String first, String second) {
+		check(first, second, first);
+	}
+	// visited should only be for the current path aka i need to turn currPath
+	// into an array list
+	public void check (String first, String second, String currPath) {
+		visited.add(first);
+		Set<String> potentialPathways = new TreeSet<>();
+		for (String neigh : map.get(first)) {
+			// base case
+			if (neigh.equals(second)) {
+				found = true;
+				paths.add(currPath + " "+ neigh);
+				}
+			else {
+				potentialPathways.add(neigh);
+			}
+		}
+		for (String neigh : potentialPathways) {
+			// recursive call
+			if (!visited.contains(neigh)) {
+				check(neigh, second, currPath + " " + neigh);
+			}
+		}
 	}
 
-	public void direct(String a, String b )
-	{
-		//check for a direct connnection
-		//add that to paths
-	}
 
-	public void check(String first, String second, String currPath)
-	{
-		//go through firsts connections and see if any of those
-		//are connected to second
-	}
+
 
 	public String toString()
 	{
--- src/graphs/graph_all_paths/AllPathsGraphRunner.java
+++ src/graphs/graph_all_paths/AllPathsGraphRunner.java
@@ -10,12 +10,16 @@ public class AllPathsGraphRunner
 {
 	public static void main( String[] args ) throws IOException
 	{
-		Scanner file = new Scanner(new File("C:\\Users\\sturner\\IdeaProjects\\DSA_Practice\\src\\graphs\\graph_all_paths\\graph3.dat"));
+		Scanner file = new Scanner(new File(
+				"C:\\Users\\zoya\\IdeaProjects\\DSA_Practice_gradebook\\src\\graphs\\graph_all_paths\\graph3.dat"));
 		int howManyTimes = file.nextInt();
 		file.nextLine();
 		for(int x=0; x<howManyTimes; x++)
 		{
-			//do some stuff
+			AllPathsGraph g = new AllPathsGraph(file.nextLine());
+			String pair = file.nextLine();
+			g.check(pair.substring(0,1), pair.substring(1,2));
+			System.out.println(g);
 		}
 	}
 }
